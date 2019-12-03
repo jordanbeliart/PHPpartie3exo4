e66e1ef00cbab5b0bad55b99ec0ab78edc8d75da
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-03 11:07:45 +0100

    exo4p3

--- index.php
+++ index.php
@@ -6,3 +6,12 @@ $departments = array(
   62 => 'Pas-de-Calais',
   80 => 'Somme');
 ?>
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Php partie 3 exo4</title>
+  </head>
+  <body>
+  </body>
+</html>
