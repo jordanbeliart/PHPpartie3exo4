29d73c7ca648bebc7321f7ac3c07fdd0583fedfe
jordanbeliart <j.beliart.jb@gmail.com>
2019-12-05 09:48:23 +0100

    correction

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 $departments = array(
-  02 => 'Aisne',
-  59 => 'Nord',
-  60 => 'Oise',
-  62 => 'Pas-de-Calais',
-  80 => 'Somme');
+  '02' => 'Aisne',
+  '59' => 'Nord',
+  '60' => 'Oise',
+  '62' => 'Pas-de-Calais',
+  '80' => 'Somme');
 ?>
 <!DOCTYPE html>
 <html lang="fr" dir="ltr">
